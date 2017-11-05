dc8d01ad8c0ec65390483100f7cbf38f72e5a20c
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-05 20:30:02 +0100

    - add extensions for string

---
+++ AlexaSkillNet/AlexaSkillNet/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AlexaSkillNet.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNeitherNullNorEmpty(this string input)
+        {
+            return !string.IsNullOrEmpty(input);
+        }
+
+        public static bool IsNullOrEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
