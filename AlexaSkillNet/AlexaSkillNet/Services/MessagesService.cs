31c588dc3f5297beac8d3b9a0d1be17631725a4b
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-05 13:32:23 +0100

    - add messages service

---
+++ AlexaSkillNet/AlexaSkillNet/Services/MessagesService.cs
@@ -0,0 +1,49 @@
+﻿using AlexaSkillNet.Extensions;
+using System.Collections.Generic;
+
+namespace AlexaSkillNet.Services
+{
+    public static class MessagesService
+    {
+        private static readonly List<string> LaunchRequestMessages = new List<string>
+        {
+            "Hallo"
+        };
+
+        private static readonly List<string> AmazonHelpIntentMessages = new List<string>
+        {
+            "Hilfe"
+        };
+
+        private static readonly List<string> AmazonStopOrCancelIntentMessages = new List<string>
+        {
+            "Stop"
+        };
+
+        private static readonly List<string> ErrorMessages = new List<string>
+        {
+            "Fehler"
+        };
+
+
+        public static string GetLaunchMessage()
+        {
+            return LaunchRequestMessages.PickRandom();
+        }
+
+        public static string GetHelpMessage()
+        {
+            return AmazonHelpIntentMessages.PickRandom();
+        }
+
+        public static string GetStopOrCancelMessage()
+        {
+            return AmazonStopOrCancelIntentMessages.PickRandom();
+        }
+
+        public static string GetErrorMessage()
+        {
+            return ErrorMessages.PickRandom();
+        }
+    }
+}
