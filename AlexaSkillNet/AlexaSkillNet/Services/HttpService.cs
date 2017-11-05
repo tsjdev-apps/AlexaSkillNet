e49eff1bff6dfd87d7a416eadf3a087c90fd3ab4
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-05 20:28:41 +0100

    - add http service

---
+++ AlexaSkillNet/AlexaSkillNet/Services/HttpService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AlexaSkillNet.Services
+{
+    public static class HttpService
+    {
+        public static async Task<TClass> GetRequestAsync<TClass>(string url)
+            where TClass : class
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(url).ConfigureAwait(false);
+                    var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    if (response.IsSuccessStatusCode)
+                        return JsonConvert.DeserializeObject<TClass>(responseString);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"HttpService | GetRequestAsync | {ex.Message}");
+                }
+            }
+
+            return default(TClass);
+        }
+    }
+}
