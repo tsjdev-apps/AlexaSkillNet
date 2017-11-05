23d984944ec63c25c4003cae6b85d02f1526f196
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-05 13:33:01 +0100

    - update function to use messages service and handle default intents

--- AlexaSkillNet/AlexaSkillNet/Function.cs
+++ AlexaSkillNet/AlexaSkillNet/Function.cs
@@ -1,27 +1,69 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
+using Alexa.NET.Response;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using AlexaSkillNet.Services;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
-
 namespace AlexaSkillNet
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        private const string AmazonStopIntent = "AMAZON.StopIntent";
+        private const string AmazonCancelIntent = "AMAZON.CancelIntent";
+        private const string AmazonHelpIntent = "AMAZON.HelpIntent";
+
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            var requestType = input.GetRequestType();
+
+            if (requestType == typeof(LaunchRequest))
+            {
+                return MakeSkillResponse(MessagesService.GetLaunchMessage(), false);
+            }
+
+            if (requestType == typeof(IntentRequest))
+            {
+                if (!(input.Request is IntentRequest intentRequest))
+                    return MakeSkillResponse(MessagesService.GetErrorMessage(), true);
+
+                switch (intentRequest.Intent.Name)
+                {
+                    case AmazonHelpIntent:
+                        return MakeSkillResponse(MessagesService.GetHelpMessage(), false);
+                    case AmazonCancelIntent:
+                    case AmazonStopIntent:
+                        return MakeSkillResponse(MessagesService.GetStopOrCancelMessage(), true);
+                    default:
+                        return MakeSkillResponse(MessagesService.GetHelpMessage(), false);
+                }
+            }
+
+            return MakeSkillResponse(MessagesService.GetErrorMessage(), true);
+        }
+
+        private static SkillResponse MakeSkillResponse(string outputSpeech, bool shouldEndSession, bool useSsml = false, string repromtText = "")
+        {
+            var response = new ResponseBody
+            {
+                ShouldEndSession = shouldEndSession,
+            };
+
+            if (useSsml)
+                response.OutputSpeech = new SsmlOutputSpeech { Ssml = outputSpeech };
+            else
+                response.OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech };
+
+            if (!string.IsNullOrEmpty(repromtText))
+                response.Reprompt = new Reprompt { OutputSpeech = new PlainTextOutputSpeech { Text = repromtText } };
+
+            var skillResponse = new SkillResponse
+            {
+                Response = response,
+                Version = "1.0"
+            };
+
+            return skillResponse;
         }
     }
 }
