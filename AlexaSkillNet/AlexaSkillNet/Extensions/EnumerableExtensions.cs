283fab330ccc6f6f98c67cf87677ec999dfdfd60
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-05 20:30:12 +0100

    - add more extensions for enumerable

--- AlexaSkillNet/AlexaSkillNet/Extensions/EnumerableExtensions.cs
+++ AlexaSkillNet/AlexaSkillNet/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace AlexaSkillNet.Extensions
 {
@@ -19,6 +20,45 @@ namespace AlexaSkillNet.Extensions
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
             return source.OrderBy(x => Guid.NewGuid());
+        }
+
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> collection)
+        {
+            return collection == null ? new ObservableCollection<T>() : new ObservableCollection<T>(collection);
+        }
+
+        public static bool HasItems<T>(this IEnumerable<T> collection)
+        {
+            return collection.IsNotNull() && collection.Any();
+        }
+
+        public static bool IsEmtpy<T>(this IEnumerable<T> collection)
+        {
+            return collection.IsNotNull() && !collection.Any();
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (var item in enumeration)
+                action(item);
+        }
+
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+                list.Add(item);
+        }
+
+        public static void Filter<T>(this IList<T> collection, IEnumerable<T> filteredCollection)
+        {
+            var enumerable = filteredCollection as T[] ?? filteredCollection.ToArray();
+            var itemsToRemove = collection.Except(enumerable).ToArray();
+
+            foreach (var item in itemsToRemove)
+                collection.Remove(item);
+
+            foreach (var item in enumerable.Where(item => !collection.Contains(item)))
+                collection.Add(item);
         }
     }
 }
