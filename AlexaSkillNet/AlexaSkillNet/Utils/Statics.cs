cc53b9f89c63104d759896e6952c86993d3c7cbf
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-09 08:55:59 +0100

    - add statics file for currently handling the intent names

---
+++ AlexaSkillNet/AlexaSkillNet/Utils/Statics.cs
@@ -0,0 +1,9 @@
+﻿namespace AlexaSkillNet.Utils
+{
+    public static class Statics
+    {
+        public const string AmazonStopIntent = "AMAZON.StopIntent";
+        public const string AmazonCancelIntent = "AMAZON.CancelIntent";
+        public const string AmazonHelpIntent = "AMAZON.HelpIntent";
+    }
+}
