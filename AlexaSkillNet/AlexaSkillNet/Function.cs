a46dea241fff14734df29827f6d0339644aeb8bb
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-09 20:49:01 +0100

    - update function to handle responseservice

--- AlexaSkillNet/AlexaSkillNet/Function.cs
+++ AlexaSkillNet/AlexaSkillNet/Function.cs
@@ -1,9 +1,7 @@
-using System;
 using Amazon.Lambda.Core;
 using Alexa.NET.Response;
 using Alexa.NET.Request;
 using Alexa.NET.Request.Type;
-using AlexaSkillNet.Enums;
 using AlexaSkillNet.Services;
 using AlexaSkillNet.Utils;
 
@@ -18,72 +16,28 @@ namespace AlexaSkillNet
 
             if (requestType == typeof(LaunchRequest))
             {
-                return MakeSkillResponse(MessagesService.GetLaunchMessage(), false);
+                return ResponseService.CreatePlainTextSkillResponse(MessagesService.GetLaunchMessage(), false);
             }
 
             if (requestType == typeof(IntentRequest))
             {
                 if (!(input.Request is IntentRequest intentRequest))
-                    return MakeSkillResponse(MessagesService.GetErrorMessage(), true);
+                    return ResponseService.CreatePlainTextSkillResponse(MessagesService.GetErrorMessage(), true);
 
                 switch (intentRequest.Intent.Name)
                 {
                     case Statics.AmazonHelpIntent:
                         var helpMessage = MessagesService.GetHelpMessage();
-                        return MakeSkillResponse(helpMessage, false, CreateCard(CardType.Simple, string.Empty, helpMessage));
+                        return ResponseService.CreatePlainTextSkillResponse(helpMessage, false);
                     case Statics.AmazonCancelIntent:
                     case Statics.AmazonStopIntent:
-                        return MakeSkillResponse(MessagesService.GetStopOrCancelMessage(), true);
+                        return ResponseService.CreatePlainTextSkillResponse(MessagesService.GetStopOrCancelMessage(), true);
                     default:
-                        return MakeSkillResponse(MessagesService.GetHelpMessage(), false);
+                        return ResponseService.CreatePlainTextSkillResponse(MessagesService.GetHelpMessage(), false);
                 }
             }
 
-            return MakeSkillResponse(MessagesService.GetErrorMessage(), true);
-        }
-
-        private static SkillResponse MakeSkillResponse(string outputSpeech, bool shouldEndSession, ICard card = null, bool useSsml = false, string repromtText = "")
-        {
-            var response = new ResponseBody
-            {
-                ShouldEndSession = shouldEndSession,
-            };
-
-            if (card != null)
-                response.Card = card;
-
-            if (useSsml)
-                response.OutputSpeech = new SsmlOutputSpeech { Ssml = outputSpeech };
-            else
-                response.OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech };
-
-            if (!string.IsNullOrEmpty(repromtText))
-                response.Reprompt = new Reprompt { OutputSpeech = new PlainTextOutputSpeech { Text = repromtText } };
-
-            var skillResponse = new SkillResponse
-            {
-                Response = response,
-                Version = "1.0"
-            };
-
-            return skillResponse;
-        }
-
-        private static ICard CreateCard(CardType cardType, string cardTitle, string cardContent, CardImage cardImage = null)
-        {
-
-            switch (cardType)
-            {
-                case CardType.Simple:
-                    return new SimpleCard { Title = cardTitle, Content = cardContent };
-                case CardType.Standard:
-                    var standardCard = new StandardCard { Title = cardTitle, Content = cardContent };
-                    if (cardImage != null)
-                        standardCard.Image = cardImage;
-                    return standardCard;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(cardType), cardType, null);
-            }
+            return ResponseService.CreatePlainTextSkillResponse(MessagesService.GetErrorMessage(), true);
         }
     }
 }
