af72e83898325fa0c06d331e2dc47885ec9079ad
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-09 20:57:14 +0100

    - add embeddedresource service

---
+++ AlexaSkillNet/AlexaSkillNet/Services/EmbeddedResourceService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace AlexaSkillNet.Services
+{
+    public static class EmbeddedResourceService
+    {
+        public static TClass GetData<TClass>(string resourcePath)
+            where TClass : class
+        {
+            try
+            {
+                var assembly = typeof(EmbeddedResourceService).GetTypeInfo().Assembly;
+                using (var stream = assembly.GetManifestResourceStream(resourcePath))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var json = reader.ReadToEnd();
+                        return JsonConvert.DeserializeObject<TClass>(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"EmbeddedResourceService | GetData | {ex.Message}");
+            }
+
+            return default(TClass);
+        }
+    }
+}
